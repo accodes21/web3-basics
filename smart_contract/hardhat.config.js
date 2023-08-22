144d5d0fb60d2730098d4386ff4160db5b80f913
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-22 14:30:34 +0530

    successful testing of smart contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/V7oty0MHNwwPedZocoayYOcRJZoGia8e
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
-  solidity: "0.8.19",
-};
+  solidity: '0.8.0',
+  networks: {
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/n4QCrfBYuDuDED5b3oLMf-Z3jXSol2F4',
+      accounts: ['c4c4cebb721372ea69f395778166b8352ac77eecfacc3b2d09647c44de23fb84'] 
+    }
+  } 
+}
