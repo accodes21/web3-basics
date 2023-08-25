0df711b8cbc845ccea9af7306e7ee3121b12af13
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-25 21:51:28 +0530

    Imported context

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,6 +2,8 @@ import { AiFillPlayCircle } from "react-icons/ai"
 import {SiEthereum} from 'react-icons/si'
 import {BsInfoCircle} from 'react-icons/bs'
 import Loader from './Loader'
+import { TransactionContext } from "../context/TransactionContext"
+import { useContext } from "react"
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
