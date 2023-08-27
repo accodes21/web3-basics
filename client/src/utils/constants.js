fdfadb7cf80850ee114bff73da527d50ec7a1a5d
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-27 17:12:29 +0530

    typo fixed

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
-export const constractABI = abi.abi;
+export const contractABI = abi.abi;
 export const contractAddress = '0xF11d510B97f398Ab9169d7a057eAb6204F3bA0d5';
