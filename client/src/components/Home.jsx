0c6daf46e15c5e2fe7041b757eb2b74bb32a87a8
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-31 18:43:30 +0530

    connected frontend to smart_contract

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import {BsInfoCircle} from 'react-icons/bs'
 import Loader from './Loader'
 import { TransactionContext } from "../context/TransactionContext"
 import { useContext } from "react"
+import { shortenAddress } from '../utils/shortenAddress'
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
@@ -20,7 +21,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Home = () => {
 
-  const {connectWallet, currentAccount, formData, handleChange, sendTransaction} = useContext(TransactionContext);
+  const {connectWallet, currentAccount, formData, handleChange, sendTransaction, isLoading} = useContext(TransactionContext);
  
   const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
@@ -83,9 +84,9 @@ const Home = () => {
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                {/* <p className="text-white font-light text-sm">
+                <p className="text-white font-light text-sm">
                   {shortenAddress(currentAccount)}
-                </p> */}
+                </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
                 </p>
@@ -100,7 +101,7 @@ const Home = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {false
+            {isLoading
               ? <Loader />
               : (
                 <button
