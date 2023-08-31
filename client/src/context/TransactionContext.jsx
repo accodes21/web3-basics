03d701461d9739948b32856d30d69dd2cc8c153b
Aarya Chopkar <aaryachopkar@gmail.com>
2023-08-31 18:42:07 +0530

    all functions completed

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -13,21 +13,41 @@ const getEthereumContract = () => {
     const transactionContract = new ethers.Contract(contractAddress, contractABI, signer)
 
     return transactionContract;
-
-    console.log({
-        provider,
-        signer,
-        transactionContract
-    });
 }
 
 export const TransactionProvider = ({children}) => {
 
     const [currentAccount, setCurrentAccount] = useState('')
     const [formData, setFormData] = useState({addresTo : '', amount : '', keyword : '', message : ''})
+    const [isLoading, setIsLoading] = useState(false)
+    const [transactionCount, setTransactionCount] = useState(localStorage.getItem('transactionCount'))
+    const [transactions, setTransactions] = useState([])
 
     const handleChange = (e, name) => {
         setFormData((prevState) => ({...prevState, [name] : e.target.value}))
+    }
+
+    const getAllTransactions = async () => {
+        try {
+            if(!ethereum) return alert("Please install Meatamask")
+            const transactionContract = getEthereumContract();
+
+            const availableTransactions = await transactionContract.getAllTransactions();
+
+            const structuredTransactions = availableTransactions.map((transaction) => ({
+                addressTo: transaction.receiver,
+                addressFrom: transaction.sender,
+                timestamp: new Date(transaction.timestamp.toNumber() * 1000).toLocaleString(),
+                message: transaction.message,
+                keyword: transaction.keyword,
+                amount: parseInt(transaction.amount._hex) / (10 ** 18)
+            }) )
+            console.log(structuredTransactions);
+
+            setTransactions(structuredTransactions)
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     const checkIfWalletIsConnected = async () => {
@@ -38,12 +58,25 @@ export const TransactionProvider = ({children}) => {
     
             if(accounts.length){
                 setCurrentAccount(accounts[0])
-                //getAllTransactions
+                getAllTransactions();
             }
             else{
                 console.log("No accounts found");
             }
             
+        } catch (error) {
+            console.log(error);
+
+            throw new Error("No ethereum object")
+        }
+    }
+
+    const checkIfTransactionsExist = async () => {
+        try {
+            const transactionContract = getEthereumContract();
+            const transactionCount = await transactionContract.getTransactionCount();
+
+            window.localStorage.setItem("transactionCount", transactionCount)
         } catch (error) {
             console.log(error);
 
@@ -69,6 +102,31 @@ export const TransactionProvider = ({children}) => {
             if(!ethereum) return alert("Please install Meatamask")
             const { addressTo, amount, keyword, message } = formData;
             const transactionContract = getEthereumContract()
+            const parsedAmount = ethers.utils.parseEther(amount)
+
+            await ethereum.request({
+                method: 'eth_sendTransaction',
+                params: [{
+                    from: currentAccount,
+                    to: addressTo,
+                    gas: '0x5208', //21000 GWEI
+                    value: parsedAmount._hex,
+                }]
+            });
+
+            const transactionHash = await transactionContract.addToBlockchain(addressTo, parsedAmount, message, keyword)
+
+            setIsLoading(true)
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            setIsLoading(false)
+            console.log(`Success - ${transactionHash.hash}`);
+
+            const transactionCount = await transactionContract.getTransactionCount();
+            setTransactionCount(transactionCount.toNumber());
+
+            window.reload()
+
         } catch (error) {
             console.log(error);
 
@@ -77,12 +135,13 @@ export const TransactionProvider = ({children}) => {
     }
 
     useEffect(() => {
-        checkIfWalletIsConnected()
+        checkIfWalletIsConnected();
+        checkIfTransactionsExist();
     }, []);
     
 
     return(
-        <TransactionContext.Provider value={{connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction}}>
+        <TransactionContext.Provider value={{connectWallet, currentAccount, formData, setFormData, handleChange, sendTransaction, transactions, isLoading}}>
             {children}
         </TransactionContext.Provider>
     )
